b91095cc523106ca5f2d3125a943714040453ca4
Cagri Esen <32637622+esencgr@users.noreply.github.com>
2018-08-16 19:45:46 +0300

    change

--- C basic code/daire/dikdörtgende işlem 2 matris.c
+++ C basic code/daire/dikdörtgende işlem 2 matris.c
@@ -13,22 +13,15 @@ int main ()
 		}
 		
 	}
-		for (i=1;i<=5;i++)
+	for (i=1;i<=5;i++)
 	{
-		for (j=1;j>=5;j++)
-		{
+		  for (j=1;j>=5;j++)
+		   {
 			
-			printf ("%d", matris[5][5]);
-			
-		}
-		
-		
-		
+		    	printf ("%d", matris[5][5]);
+		  	
+		   }	
 	}
-	
-	
-	
-	
 	
 	return 0;
 	
