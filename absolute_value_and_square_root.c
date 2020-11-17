1ace67dd42d8290cf1e484016408f56f875d89fd
Cagri Esen <esencgr@gmail.com>
2020-11-17 18:26:15 +0300

    add

--- absolute_value_and_square_root.c
+++ absolute_value_and_square_root.c
@@ -8,16 +8,16 @@ int main (){
 	double x;
 	printf ("sayi giriniz:");
 	scanf ("%d",&sayi);
-
+    
 	printf ("mutlak sayi :%d dir\n ",mutlak(sayi));
-	x=sqrt(mutlak (sayi));
+	x = sqrt( (mutlak (sayi)),2 );
 	printf ("%d sayisinin karekoku :%2f",mutlak(sayi),x);
 
 	return 0;
 }
 
 int mutlak (int y){
-
+	
 	if (y<(-1))
 	y=y*(-1);
 	else
